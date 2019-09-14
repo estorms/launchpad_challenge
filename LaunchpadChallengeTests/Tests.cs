603c7422c347c49ddddddf8b54e5bbf02788d273
Elizabeth Sanger <libsang@gmail.com>
2019-09-14 17:17:57 -0500

    removed bin and .idea files

---
+++ LaunchpadChallengeTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace launchpad_challenge.LaunchpadChallengeTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestThatTestsAreWorking()
+        {
+            Assert.True(true);
+        }
+    }
+}
